a6cb755f7f5da8e74c213ce8bcd4ad09e4aeb069
Kevin Chevalier <kevin.chevalier10@gmail.com>
2019-11-12 01:50:11 -1000

    Kevin Changes

--- kevin/database/proc.sql
+++ kevin/database/proc.sql
@@ -1,9 +1,16 @@
-DROP PROCEDURE IF EXISTS add_entry;
+DROP PROCEDURE IF EXISTS add_effective_rate;
+DROP PROCEDURE IF EXISTS add_max_income;
 
 DELIMITER $$
-CREATE PROCEDURE add_entry(month_num CHAR(2), year_num INT, island INT,kwh INT,cost_kwh FLOAT,type INT)
+CREATE PROCEDURE add_effective_rate(month_num CHAR(2), year_num INT, island INT,kwh INT,cost_kwh FLOAT,type INT)
 BEGIN
     INSERT INTO `effective_rate` (`rate_id`, `month_num`, `year_num`, `island`, `kwh`, `cost_kwh`, `type`)
     VALUES(NULL, month_num, year_num, island,kwh,cost_kwh,type);
+END$$
+
+CREATE PROCEDURE add_max_income(island INT, year_num INT,num_person INT,max_income INT)
+BEGIN
+    INSERT INTO `max_income` (`income_id`, `island`, `year_num`, `num_person`, `max_income`)
+    VALUES(NULL, island, year_num, num_person,max_income);
 END$$
 DELIMITER ;
--- kevin/database/reset.sql
+++ kevin/database/reset.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS effective_rate;
+DROP TABLE IF EXISTS max_income;
 
 CREATE TABLE effective_rate (
     rate_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -8,4 +9,12 @@ CREATE TABLE effective_rate (
     kwh INT NOT NULL,
     cost_kwh FLOAT NOT NULL,
     type INT NOT NULL
+);
+
+CREATE TABLE max_income (
+    income_id INT AUTO_INCREMENT PRIMARY KEY,
+    island INT NOT NULL,
+    year_num INT NOT NULL,
+    num_person INT NOT NULL,
+    max_income INT NOT NULL
 );
